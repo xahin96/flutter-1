124d655c94324f337b85924d1215c6bea2f65ba2
zahin <xahin96@gmail.com>
2023-03-31 03:30:52 +0600

    Floating button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-main(){
+main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home:HomeActivity());
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.brown),
+      darkTheme: ThemeData(primarySwatch: Colors.brown),
+      color: Colors.lightGreen,
+      debugShowCheckedModeBanner: false,
+      home: HomeActivity(),
+    );
   }
 }
 
-class HomeActivity extends StatelessWidget{
+class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
+
+  notificationOnClick(message, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(message)));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('First App'),
+        title: const Text('First Application'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                notificationOnClick('No notifications', context);
+              },
+              icon: const Icon(Icons.notifications))
+        ],
       ),
-      body: Text('Hello'),
+      body: const Text('Hello'),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.black54,
+        onPressed: () {
+          notificationOnClick('Add clicked', context);
+        },
+      ),
     );
   }
 }
