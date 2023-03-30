793417347e73c764f00ebf2ae43f901c9a71408b
zahin <xahin96@gmail.com>
2023-03-31 02:30:46 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/zahin/app1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.zahin.app1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home:HomeActivity());
+  }
+}
+
+class HomeActivity extends StatelessWidget{
+  const HomeActivity({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+      ),
+      body: Text('Hello'),
+    );
+  }
+}
