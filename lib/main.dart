103962a2177aee8415ecf9024bf7a5185beb81e9
zahin <xahin96@gmail.com>
2023-04-08 01:39:41 +0600

    Shahriar | Dev-SimpleForm | form elements design changed

--- lib/main.dart
+++ lib/main.dart
@@ -91,7 +91,7 @@ class HomeActivity extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.all(10),
+            padding: EdgeInsets.all(12),
             child: TextField(
               decoration: InputDecoration(
                   border: OutlineInputBorder(), labelText: 'Email Address'),
